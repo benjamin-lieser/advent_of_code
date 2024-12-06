5849b4a9162f43be080ac4bcd85c850c96f45b9e
Benjamin Lieser <benjamin.lieser@mpinat.mpg.de>
2024-12-06 12:25:18 +0100

    6 2

--- aoc2024/src/bin/6_1.rs
+++ aoc2024/src/bin/6_1.rs
@@ -2,7 +2,7 @@ use advent_of_code::*;
 
 fn main() {
     let input = get_input(2024, 6);
-    
+
     let mut acc = 0;
 
     let mut grid = read_grid(&input);
@@ -10,42 +10,49 @@ fn main() {
     let h = grid.len() as isize;
     let w = grid[0].len() as isize;
 
-    let mut pos : Pos = (0,0);
-    let mut dir : Dir = Dir::Up;
-
+    let mut start: Pos = (0, 0);
+    
     for i in 0..h {
         for j in 0..w {
             if grid[i as usize][j as usize] == '^' {
-                pos = (j,i);
+                start = (j, i);
             }
         }
     }
 
-    loop {
-        *index_mut(&mut grid, pos).unwrap() = 'X';
-        let next = pos + dir;
-        match index(&grid, next) {
-            Some('#') => {
-                dir = dir.turn_r();
-            },
-            None => {
-                break;
-            },
-            Some(_) => {
-                pos = next;
-            },
+    let is_loop = |p: Pos| -> bool {
+        let mut grid = grid.clone();
+        *index_mut(&mut grid, p).unwrap() = '#';
+        let mut pos = start;
+        let mut dir = Dir::Up;
+        for _ in 0..h*w+10 {
+            let next = pos + dir;
+            match index(&grid, next) {
+                Some('#') => {
+                    dir = dir.turn_r();
+                }
+                None => {
+                    return false;
+                }
+                Some(_) => {
+                    pos = next;
+                }
+            }
         }
-    }
+
+        return true;
+    };
 
     for i in 0..h {
         for j in 0..w {
-            if index(&grid, (i,j)) == Some('X') {
+            if (i,j) == start {
+                continue;
+            }
+            if is_loop((j, i)) {
                 acc += 1;
             }
         }
     }
 
-    println!("{}", grid.iter().map(|x| x.iter().collect::<String>()).collect::<Vec<String>>().join("\n"));
-    
     println!("{}", acc);
 }
