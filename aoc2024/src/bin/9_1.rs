92c976624e230a490b92086feaf43e7cc8a7e3e1
Benjamin Lieser <benjamin.lieser@mpinat.mpg.de>
2024-12-09 09:53:43 +0100

    9_1

--- aoc2024/src/bin/9_1.rs
+++ aoc2024/src/bin/9_1.rs
@@ -1,32 +1,46 @@
 use advent_of_code::*;
 
-fn can_be_solved(result : int, numbers : &[int], current : int) -> bool {
-    if numbers.len() == 0 {
-        return result == current;
-    }
-    if can_be_solved(result, &numbers[1..], current + numbers[0]) {
-        return true;
-    }
-    if can_be_solved(result, &numbers[1..], current * numbers[0]) {
-        return true;
-    }
-    let concat : int= format!("{}{}", current, numbers[0]).parse().unwrap();
-    if can_be_solved(result, &numbers[1..], concat) {
+fn fragment(disk: &mut [int]) -> bool {
+    let first_free = disk.iter().position(|&x| x == -1).unwrap();
+    let last_block = disk.iter().rposition(|&x| x != -1).unwrap();
+    if first_free < last_block {
+        disk.swap(first_free as usize, last_block as usize);
         return true;
     }
     false
 }
 
 fn main() {
-    let input = get_input(2024, 7);
+    let input = get_input(2024, 9);
 
     let mut acc = 0;
 
-    for line in input.lines() {
-        let (result, numbers) = line.so(":");
-        let numbers : Vec<int> = numbers.scast("");
-        if can_be_solved(result.parse().unwrap(), &numbers[1..], numbers[0]) {
-            acc += result.parse::<int>().unwrap();
+    let mut disk = vec![];
+
+    let mut block = true;
+
+    let mut block_id: int = 0;
+
+    for char in input.trim().chars() {
+        let digit = char.to_digit(10).unwrap();
+        if block {
+            for _ in 0..digit {
+                disk.push(block_id);
+            }
+            block_id += 1;
+        } else {
+            for _ in 0..digit {
+                disk.push(-1);
+            }
+        }
+        block = !block;
+    }
+
+    while fragment(&mut disk) {}
+
+    for (i, &block) in disk.iter().enumerate() {
+        if block != -1 {
+            acc += block * i as int;
         }
     }
 
