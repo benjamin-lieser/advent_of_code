d21087ad880b265fb56e20b2c633d09e327d0db3
Benjamin Lieser <benjamin.lieser@mpinat.mpg.de>
2024-12-09 19:49:36 +0100

    9_2

--- aoc2024/src/bin/9_2.rs
+++ aoc2024/src/bin/9_2.rs
@@ -1,7 +1,34 @@
 use advent_of_code::*;
 
-fn fragment(blocks: &mut [int], free: &mut [int]) -> bool {
-    
+fn fragment(disk: &mut [int], file_id: int) {
+    let first_file = disk.iter().position(|&x| x == file_id).unwrap();
+    let last_file = disk.iter().rposition(|&x| x == file_id).unwrap();
+
+    let file_size = last_file as int - first_file as int + 1;
+
+    let mut free_begin = 0;
+    let mut free_length = 0;
+
+    for i in 0..disk.len() {
+        if disk[i] == -1 {
+            free_length += 1;
+        } else {
+            free_length = 0;
+            free_begin = i + 1;
+        }
+        if free_length == file_size {
+            break;
+        }
+    }
+
+    if free_length >= file_size && free_begin < first_file {
+        for i in 0..file_size {
+            disk.swap(
+                first_file as usize + i as usize,
+                free_begin as usize + i as usize,
+            );
+        }
+    }
 }
 
 fn main() {
@@ -9,8 +36,6 @@ fn main() {
 
     let mut acc = 0;
 
-    let mut blocks = vec![];
-    let mut free = vec![];
     let mut disk = vec![];
 
     let mut block = true;
@@ -18,15 +43,13 @@ fn main() {
     let mut block_id: int = 0;
 
     for char in input.trim().chars() {
-        let digit = char.to_digit(10).unwrap() as int;
+        let digit = char.to_digit(10).unwrap();
         if block {
             for _ in 0..digit {
                 disk.push(block_id);
             }
-            blocks.push(digit);
             block_id += 1;
         } else {
-            free.push((digit, disk.len()));
             for _ in 0..digit {
                 disk.push(-1);
             }
@@ -34,7 +57,9 @@ fn main() {
         block = !block;
     }
 
-    while fragment(&mut disk) {}
+    for i in (0..block_id).rev() {
+        fragment(&mut disk, i as int);
+    }
 
     for (i, &block) in disk.iter().enumerate() {
         if block != -1 {
