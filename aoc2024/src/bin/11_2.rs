3c145e419e7dce45525f8626edc3f11315642bf8
Benjamin Lieser <benjamin.lieser@mpinat.mpg.de>
2024-12-11 07:59:49 +0100

    use the hash map correctly

--- aoc2024/src/bin/11_2.rs
+++ aoc2024/src/bin/11_2.rs
@@ -17,12 +17,7 @@ fn apply(stones : HashMap<int, int>) -> HashMap<int, int> {
     for stone in stones {
         let new = rule(stone.0);
         for n in new {
-            if new_stones.contains_key(&n) {
-                let count = new_stones.get(&n).unwrap();
-                new_stones.insert(n, count + stone.1);
-            } else {
-                new_stones.insert(n, stone.1);
-            }
+            *new_stones.entry(n).or_insert(0) += stone.1;
         }
     }
     new_stones
@@ -37,12 +32,7 @@ fn main() {
 
 
     for stone in stones {
-        if stones_map.contains_key(&stone) {
-            let count = stones_map.get(&stone).unwrap();
-            stones_map.insert(stone, count + 1);
-        } else {
-            stones_map.insert(stone, 1);
-        }
+        *stones_map.entry(stone).or_insert(0) += 1;
     }
     for _ in 0..75 {
         stones_map = apply(stones_map);
