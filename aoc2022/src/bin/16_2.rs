b9c891e08fcb7dc7f9e78baeaa11fc7f9b3dfb63
Benjamin Lieser <benjamin.lieser@mpinat.mpg.de>
2024-12-18 12:17:46 +0100

    can run, but slow and needs a lot of mem

--- aoc2022/src/bin/16_2.rs
+++ aoc2022/src/bin/16_2.rs
@@ -1,19 +1,23 @@
 use advent_of_code::*;
 
-static GRAPH: Global<UnGraphMap<usize, ()>> = Global::new();
+static GRAPH: Global<UnGraphMap<u8, ()>> = Global::new();
 static FLOWS: Global<Vec<int>> = Global::new();
+static MEMO: Global<HashMap<(smallbitset::Set64, u8,u8,u8), int>> = Global::new();
 
-#[memoize::memoize]
-fn max_release(pos1: usize, pos2: usize, time_left: usize, open: Vec<bool>) -> int {
+fn max_release(pos1: u8, pos2: u8, time_left: u8, open: smallbitset::Set64) -> int {
     if time_left == 0 {
         return 0;
+    }
+
+    if let Some(&result) = MEMO.borrow().get(&(open, pos1, pos2, time_left)) {
+        return result;
     }
 
     let release = FLOWS
         .borrow()
         .iter()
-        .zip(&open)
-        .map(|(flow, open)| if *open { flow } else { &0 })
+        .enumerate()
+        .map(|(idx, flow, )| if open.contains(idx) { flow } else { &0 })
         .sum::<int>();
 
     let mut max = 0;
@@ -22,15 +26,15 @@ fn max_release(pos1: usize, pos2: usize, time_left: usize, open: Vec<bool>) -> i
         for next2 in GRAPH.borrow().neighbors(pos2) {
             let mut new_open = open.clone();
             if next1 == pos1 {
-                if FLOWS.borrow()[next1] > 0 {
-                    new_open[next1] = true;
+                if FLOWS.borrow()[next1 as usize] > 0 {
+                    new_open = new_open.add(next1 as usize);
                 } else {
                     continue;
                 }
             }
             if next2 == pos2 {
-                if FLOWS.borrow()[next2] > 0 {
-                    new_open[next2] = true;
+                if FLOWS.borrow()[next2 as usize] > 0 {
+                    new_open = new_open.add(next2 as usize);
                 } else {
                     continue;
                 }
@@ -39,10 +43,17 @@ fn max_release(pos1: usize, pos2: usize, time_left: usize, open: Vec<bool>) -> i
             max = max.max(release + future);
         }
     }
+
+    MEMO.borrow_mut().insert((open, pos1, pos2, time_left), max);
     max
 }
 
 fn main() {
+
+    MEMO.set(HashMap::new());
+
+    dbg!(std::mem::size_of::<smallbitset::Set64>());
+
     let input = get_input(2022, 16);
 
     let mut indexer = Indexer::new();
@@ -55,15 +66,14 @@ fn main() {
         let [flow] = get_all_int(&line);
         let nodes = get_all_matches(&line, "[A-Z][A-Z]")
             .iter()
-            .map(|&x| indexer.get(x))
+            .map(|&x| indexer.get(x) as u8)
             .collect::<Vec<_>>();
         for node in &nodes {
             edges.push((*node, nodes[0]));
         }
-        flows[nodes[0]] = flow;
+        flows[nodes[0] as usize] = flow;
     }
 
-    let open = vec![false; flows.len()];
     FLOWS.set(flows);
 
     let graph = UnGraphMap::<_, ()>::from_edges(edges);
@@ -72,10 +82,10 @@ fn main() {
     println!(
         "{}",
         max_release(
-            indexer.get("AA"),
-            indexer.get("AA"),
+            indexer.get("AA") as u8,
+            indexer.get("AA") as u8,
             26,
-            open
+            smallbitset::Set64::empty()
         )
     );
 }
