b7fc9ba79c466a5c66ba7eca31845b2ec5493594
Benjamin Lieser <benjamin.lieser@mpinat.mpg.de>
2024-12-20 11:59:05 +0100

    backport part two, into part 1

--- aoc2024/src/bin/20_1.rs
+++ aoc2024/src/bin/20_1.rs
@@ -1,5 +1,4 @@
 use advent_of_code::*;
-use petgraph::visit::EdgeRef;
 
 fn main() {
     let input = get_input(2024, 20);
@@ -11,45 +10,32 @@ fn main() {
     grid.set(start, '.');
     grid.set(end, '.');
 
-    let mut graph = UnGraphMap::<Pos,int>::new();
+    let mut path = vec![start];
+    let mut current = start;
+    let mut last = start;
 
-    for pos in grid.positions() {
-        if grid.get(pos) == Some('#') {
-            continue;
-        }
+    while current != end {
+        let mut next = (0,0);
         for dir in DIRS {
-            if grid.get(pos + dir) == Some('.') {
-                graph.add_edge(pos, pos + dir, 1);
+            if current + dir != last && grid.get(current + dir) == Some('.') {
+                next = current + dir;
             }
         }
-    }
-    let dists = petgraph::algo::dijkstra(&graph, start, None, |e| *e.weight());
-
-    let orig = dists.get(&end).unwrap();
-
-    let mut cheats : HashMap<int, Vec<(Pos, Pos)>> = HashMap::new();
-
-    for pos in grid.positions() {
-        if grid.get(pos) == Some('#') {
-            continue;
-        }
-        for dir in DIRS {
-            let pos2 = pos + 2 * dir;
-            if grid.get(pos2) == Some('.') {
-                let mut graph2 = graph.clone();
-                graph2.add_edge(pos, pos2, 2);
-                let dists2 = petgraph::algo::dijkstra(&graph2, start, Some(end), |e| *e.weight());
-                let dist = dists2.get(&end).unwrap();
-                cheats.entry(orig - dist).or_default().push((pos, pos2));
-            }
-        }
+        path.push(next);
+        last = current;
+        current = next;
     }
 
     let mut acc = 0;
 
-    for dist in cheats.keys() {
-        if *dist >= 100 {
-            acc += cheats[dist].len() / 2;
+    for i in 0..path.len() {
+        for j in i+1..path.len() {
+            let a = path[i];
+            let b = path[j];
+            let dist = manhattan(a, b);
+            if dist <= 2 && (j - i) as int - dist >= 100 {
+                acc += 1;
+            }
         }
     }
 
